da1f4cbcea3c8a0beac1594fbfa76374919c1145
Rahul-Verma <94426476+starcaptian@users.noreply.github.com>
2022-03-09 00:39:52 +0530

    commit

--- Todolist/index.js
+++ Todolist/index.js
@@ -1,9 +1,29 @@
-let screen = document.getElementById('screen');
+let screen=document.getElementById('screen');
 let addbuddy = document.getElementById('submit');
 let addlist = document.getElementById('ourlist');
-addbuddy.addEventListener('click',()=>{
-    let list = document.createElement('li');
-    list.innerHTML = screen.value;
-    addlist.appendChild(list);
+addbuddy.addEventListener('click', () => {
+    document.getElementById('emptyid').innerText = ""
     
+    if (screen.value == "") {
+
+        document.getElementById('emptyid').innerText = "please enter value";
+
+    } else {
+        
+        let list = document.createElement('li');
+        let deletlist = document.createElement('button');
+        deletlist.textContent = "Remove";
+
+        list.textContent = screen.value;
+        addlist.appendChild(list);
+        addlist.appendChild(deletlist);
+        deletlist.addEventListener('click', () => {
+            addlist.removeChild(list);
+            addlist.removeChild(deletlist);
+        })
+
+        screen.value=""
+    
+    }
 })
+
