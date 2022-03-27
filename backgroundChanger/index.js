55d5a4f81e4801aeb8d3d25625d6eeaeb52112bf
Rahul-Verma <94426476+rvstarcaptain@users.noreply.github.com>
2022-03-27 23:21:04 +0530

    commit

---
+++ backgroundChanger/index.js
@@ -0,0 +1,24 @@
+document.getElementById('pink').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'pink';
+    document.getElementById('text').style.color = 'black';
+    document.getElementsByClassName('colorpicker')[0].style,backgroundColor = 'lightblue'
+
+})
+document.getElementById('black').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'black';
+    document.getElementById('text').style.color = 'white';
+    document.getElementById('head').style.color = 'white';
+
+})
+document.getElementById('blue').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'blue';
+    document.getElementById('text').style.color = 'white';
+    document.getElementById('head').style.color = 'white';
+
+})
+document.getElementById('aqua').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'aqua';
+    document.getElementById('text').style.color = 'black';
+    document.getElementById('head').style.color = 'black';
+
+})
